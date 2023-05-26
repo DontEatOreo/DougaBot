c2d6465021ed858d283af9d913170c5010b63470
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2023-05-26 20:30:29 +0300

    Version 1.0.1 Switch to using appsettings.json as config

---
+++ DougaBot/AppSettings.cs
@@ -0,0 +1,6 @@
+namespace DougaBot;
+
+public class AppSettings
+{
+    
+}
--- DougaBot/Program.cs
+++ DougaBot/Program.cs
@@ -10,12 +10,11 @@ using DougaBot.Services.Speed;
 using DougaBot.Services.Trim;
 using DougaBot.Services.Video;
 using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Serilog;
-
-await Globals.CheckForYtdlp().ConfigureAwait(false);
-await Globals.CheckForFFmpeg().ConfigureAwait(false);
 
 DiscordSocketConfig socketConfig = new()
 {
@@ -23,40 +22,51 @@ DiscordSocketConfig socketConfig = new()
                      ~GatewayIntents.GuildInvites &
                      ~GatewayIntents.GuildScheduledEvents,
 };
+AsyncKeyedLocker<string> locker = new(o =>
+{
+    o.PoolSize = 1;
+    o.PoolInitialFill = 1;
+    if (Environment.ProcessorCount == 1)
+        return;
+    o.PoolSize = Environment.ProcessorCount * 2;
+    o.PoolInitialFill = Environment.ProcessorCount * 2;
+});
+DiscordSocketClient client = new(socketConfig);
+
+var logsFolder = Path.Combine(Environment.CurrentDirectory, "logs");
+var logPath = Path.Combine(logsFolder, "log.txt");
 
 var host = Host.CreateDefaultBuilder()
     .ConfigureServices(
         (_, services) =>
         {
-            services.AddHttpClient<CatBoxHttpClient>();
-            services.AddSingleton(_ => new DiscordSocketClient(socketConfig));
+            services.AddHttpClient<CatBoxClient>();
+
+            services.AddSingleton(locker);
+            services.AddSingleton(client);
             services.AddSingleton<InteractionService>();
             services.AddSingleton<InteractionHandler>();
             services.AddSingleton<Globals>();
             services.AddSingleton<RateLimitService>();
+
             services.AddTransient<IContentTypeProvider, FileExtensionContentTypeProvider>();
             services.AddTransient<IVideoService, VideoService>();
             services.AddTransient<IAudioService, AudioService>();
             services.AddTransient<ISpeedService, SpeedService>();
             services.AddTransient<ITrimService, TrimService>();
-            services.AddSingleton(new AsyncKeyedLocker<string>(o =>
-            {
-                o.PoolSize = 1;
-                o.PoolInitialFill = 1;
-                if (Environment.ProcessorCount == 1)
-                    return;
-                o.PoolSize = Environment.ProcessorCount * 2;
-                o.PoolInitialFill = Environment.ProcessorCount * 2;
-            }));
         }
     )
-    .UseSerilog((hostingContext, _, loggerConfiguration) => loggerConfiguration
-        .ReadFrom.Configuration(hostingContext.Configuration)
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath);
+        config.AddJsonFile("appsettings.json", true, true);
+        config.AddEnvironmentVariables();
+    })
+    .UseSerilog((context, _, loggerConfiguration) => loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
         .Enrich.FromLogContext()
         .WriteTo.Console()
-        .WriteTo.File(
-            Path.Combine(Environment.GetEnvironmentVariable("LOG_PATH") ??
-                         Path.Combine(Environment.CurrentDirectory, "logs"), "log.txt"),
+        .WriteTo.File(logPath,
             rollingInterval: RollingInterval.Day,
             retainedFileCountLimit: 7,
             shared: true,
@@ -70,14 +80,19 @@ var socketClient = provider.GetRequiredService<DiscordSocketClient>();
 await provider.GetRequiredService<InteractionHandler>().InitializeAsync();
 
 var globalTasks = provider.GetRequiredService<Globals>();
+var appSettings = provider.GetRequiredService<IOptions<AppSettings>>();
+
+await globalTasks.CheckForYtdlp();
+await globalTasks.CheckForFFmpeg();
 
 socketClient.Log += globalTasks.LogAsync;
 interactionService.Log += globalTasks.LogAsync;
 
 // Registers commands globally
-if (Convert.ToBoolean(Environment.GetEnvironmentVariable("REGISTER_GLOBAL_COMMANDS")))
+if (appSettings.Value.RegisterGlobalCommands ?? false)
     socketClient.Ready += async () => await interactionService.RegisterCommandsGloballyAsync();
 
-await socketClient.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DOUGA_TOKEN"));
+var dougaToken = Environment.GetEnvironmentVariable("DOUGA_TOKEN");
+await socketClient.LoginAsync(TokenType.Bot, dougaToken);
 await socketClient.StartAsync();
 await Task.Delay(-1);
